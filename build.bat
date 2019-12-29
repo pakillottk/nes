d512248c75f4327de8d2769ab480e6a6cdf41e6b
Gázquez Megías, Francisco Manuel <fmgm0005@red.ujaen.es>
2019-12-29 21:03:16 +0100

    Release build

--- build.bat
+++ build.bat
@@ -1,6 +1,12 @@
 @echo off
 
+::Debug flags
+::set compiler_flags=/Zi /D_DEBUG /Od
+
+::Release flags
+set compiler_flags=/O2
+
 pushd ".\build"
-    cl /Zi /D_USRDLL /D_WINDLL ..\src\nes.cpp opengl32.lib glew32.lib /link /DLL /OUT:NES.dll 
-    cl /Zi /DHOT_RELOAD=1 ..\src\win32_nes.cpp user32.lib gdi32.lib comdlg32.lib opengl32.lib glew32.lib
+    cl %compiler_flags% /D_USRDLL /D_WINDLL ..\src\nes.cpp opengl32.lib glew32.lib /link /DLL /OUT:NES.dll 
+    cl %compiler_flags% ..\src\win32_nes.cpp user32.lib gdi32.lib comdlg32.lib opengl32.lib glew32.lib
 popd
